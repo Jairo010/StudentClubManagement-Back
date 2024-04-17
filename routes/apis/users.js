c708f02017f9cba6b727b112e51a3574624755c5
Jairo Freire <jairofreireortiz10@gmail.com>
2024-04-17 00:14:15 -0500

    The configuration of the routes has been established

---
+++ routes/apis/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const supabase = require("../../config/dbConnection");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+
+})
+
+module.exports = router;
