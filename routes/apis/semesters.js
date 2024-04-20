7820753a28aaa03746c46f2fd26a8d4f22128b6d
Jairo Freire <jairofreireortiz10@gmail.com>
2024-04-20 15:52:19 -0500

    The file for the semester routes has been created.

---
+++ routes/apis/semesters.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const supabase = require("../../config/dbConnection");
+const router = express.Router();
+
+router.get("/semester", async (req, res) => {
+    
+})
